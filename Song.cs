a0dc5d10d46f64155eb2d7b52730c0d890f3d0c9
xingmyg <1213099@student.roc-nijmegen.nl>
2026-05-20 14:18:13 +0200

    Eerste opzet voor de classes
    Basis structuur voor de classes om vanuit verder te werken

---
+++ Song.cs
@@ -0,0 +1,29 @@
+public class Song : IPlayable
+{
+	// Properties
+	public string Title { get; set; }
+	public List<Artist> Artists { get; set; }
+	public Genre SongGenre { get; set; }
+	public int Duur { get; set; }
+
+	// Constructor
+	public Song(string title, List<Artist> artists, int duur, Genre songGenre)
+	{
+		Title = title;
+		Artists = artists;
+		Duur = duur;
+		SongGenre = songGenre;
+	}
+
+	// IPlayable methoden
+	public void Play() { }
+	public void Pause() { }
+	public void Next() { }
+	public void Stop() { }
+	public int Length { get; set; }
+
+	public override string ToString()
+	{
+		return $"Song: {Title}, Duur: {Duur}, Genre: {SongGenre}";
+	}
+}
---
+++ SuperUser.cs
@@ -0,0 +1,34 @@
+public class SuperUser
+{
+
+    // Constructor
+    public SuperUser()
+    {
+    }
+
+    // Methoden
+    public void AddFriend(Person person)
+    {
+    }
+
+    public void RemoveFriend(Person person)
+    {
+    }
+
+    public PlayList CreatePlayList(string name)
+    {
+        return null;
+    }
+
+    public void RemovePlayList(int index)
+    {
+    }
+
+    public void AddToPlayList(iPlayable playable)
+    {
+    }
+
+    public void RemoveFromPlayList(iPlayable playable)
+    {
+    }
+}
