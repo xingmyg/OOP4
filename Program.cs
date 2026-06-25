a1302618454a8752194e8ff464593674155ae2bb
xingmyg <1213099@student.roc-nijmegen.nl>
2026-06-25 11:40:40 +0200

    Vrienden verwijderen - U13

--- Program.cs
+++ Program.cs
@@ -19,58 +19,64 @@ List<Song> songs = new List<Song>
     song1, song2, song3, song4, song5, song6, song7, song8, song9, song10
 };
 
-// Albums
+//albums
 List<Album> albums = new List<Album>
 {
     new Album(new List<Artist> { artist1 }, "Divide", new List<Song> { song1, song2, song3 }),
     new Album(new List<Artist> { artist1 }, "Plus", new List<Song> { song4, song5, song6 }),
 };
 
-//user
-SuperUser user = new SuperUser("Jij");
-Client client = new Client(new List<Person>(), albums, songs);
-client.SetActiveUser(user);
+SuperUser user1 = new SuperUser("Jij");
+SuperUser user2 = new SuperUser("Donald");
+SuperUser user3 = new SuperUser("Maud");
+SuperUser user4 = new SuperUser("Joris");
+SuperUser user5 = new SuperUser("Lisa");
 
-bool running = true;
-while (running)
+List<Person> users = new List<Person> { user2, user3, user4, user5 };
+
+Client client = new Client(users, albums, songs);
+client.SetActiveUser(user1);
+
+while (true)
 {
     Console.WriteLine("\nWat wil je doen?");
     Console.WriteLine("1. Albums bekijken");
     Console.WriteLine("2. Alle nummers bekijken");
     Console.WriteLine("3. Playlist maken");
-    Console.WriteLine("4. Mijn playlists bekijken");
+    Console.WriteLine("4. Vrienden beheren");
     Console.WriteLine("0. Stoppen");
 
-    int hoofdKeuze = int.Parse(Console.ReadLine() ?? "0");
+    int HoofdKeuze = int.Parse(Console.ReadLine() ?? "0");
 
-    if (hoofdKeuze == 0)
+    if (HoofdKeuze == 0)
     {
-        running = false;
+        Console.WriteLine("Tot ziens!");
+        break;
     }
-    else if (hoofdKeuze == 1)
+    else if (HoofdKeuze == 1)
     {
         Console.WriteLine("\nKies een album:");
         for (int i = 0; i < albums.Count; i++)
             Console.WriteLine($"{i + 1}. {albums[i]}");
 
-        int albumKeuze = int.Parse(Console.ReadLine() ?? "0");
+        int AlbumKeuze = int.Parse(Console.ReadLine() ?? "0");
 
-        if (albumKeuze >= 1 && albumKeuze <= albums.Count)
-        {
-            Album gekozenAlbum = albums[albumKeuze - 1];
-            gekozenAlbum.Play();
-        }
+        if (AlbumKeuze >= 1 && AlbumKeuze <= albums.Count)
+            albums[AlbumKeuze - 1].Play();
     }
-    else if (hoofdKeuze == 2)
+    else if (HoofdKeuze == 2)
     {
         Console.WriteLine("\nKies een nummer:");
         for (int i = 0; i < songs.Count; i++)
             Console.WriteLine($"{i + 1}. {songs[i]}");
 
-        int nummerKeuze = int.Parse(Console.ReadLine() ?? "0");
+        int NummerKeuze = int.Parse(Console.ReadLine() ?? "0");
 
-        if (nummerKeuze >= 1 && nummerKeuze <= songs.Count)
+        if (NummerKeuze >= 1 && NummerKeuze <= songs.Count)
         {
+            client.SelectSong(NummerKeuze - 1);
+            client.Play();
+
             Console.WriteLine("Volgende nummer? (j/n)");
             if (Console.ReadLine() == "j")
                 client.NextSong();
@@ -84,38 +90,95 @@ while (running)
                 Console.WriteLine("Nummer wordt niet gestopt.");
         }
     }
-    else if (hoofdKeuze == 3)
+    else if (HoofdKeuze == 3)
     {
         Console.WriteLine("\nGeef je playlist een naam:");
-        string? input = Console.ReadLine();
-        string naam = string.IsNullOrWhiteSpace(input) ? "Mijn Playlist" : input;
-        client.CreatePlaylist(naam);
+        string? Input = Console.ReadLine();
+        string Naam = string.IsNullOrWhiteSpace(Input) ? "Mijn Playlist" : Input;
+        client.CreatePlaylist(Naam);
 
-        bool doorgaan = true;
-        while (doorgaan)
+        bool Doorgaan = true;
+        while (Doorgaan)
         {
             Console.WriteLine("\nKies een nummer om toe te voegen (of 0 om te stoppen):");
             for (int i = 0; i < songs.Count; i++)
                 Console.WriteLine($"{i + 1}. {songs[i]}");
 
-            int songKeuze = int.Parse(Console.ReadLine() ?? "0");
+            int SongKeuze = int.Parse(Console.ReadLine() ?? "0");
 
-            if (songKeuze == 0)
+            if (SongKeuze == 0)
             {
-                doorgaan = false;
+                Doorgaan = false;
             }
-            else if (songKeuze >= 1 && songKeuze <= songs.Count)
+            else if (SongKeuze >= 1 && SongKeuze <= songs.Count)
             {
-                client.AddToPlaylist(songKeuze - 1);
-                Console.WriteLine($"{songs[songKeuze - 1].Title} toegevoegd!");
+                client.AddToPlaylist(SongKeuze - 1);
+                Console.WriteLine($"{songs[SongKeuze - 1].Title} toegevoegd!");
             }
         }
 
         client.ShowSongsInPlaylist(0);
     }
-    else if (hoofdKeuze == 4)
+    else if (HoofdKeuze == 4)
     {
-        client.ShowPlaylists();
+        while (true)
+        {
+            Console.WriteLine("\nWat wil je doen?");
+            Console.WriteLine("1. Vriend toevoegen");
+            Console.WriteLine("2. Vriend verwijderen");
+            Console.WriteLine("3. Vrienden bekijken");
+            Console.WriteLine("0. Terug naar hoofdmenu");
+
+            int VriendKeuze = int.Parse(Console.ReadLine() ?? "0");
+
+            if (VriendKeuze == 1)
+            {
+                Console.WriteLine("\nKies een gebruiker om toe te voegen als vriend:");
+                for (int i = 0; i < users.Count; i++)
+                    Console.WriteLine($"{i + 1}. {users[i].Naam}");
+
+                int UserKeuze = int.Parse(Console.ReadLine() ?? "0");
+
+                if (UserKeuze >= 1 && UserKeuze <= users.Count)
+                {
+                    client.AddFriend(UserKeuze - 1);
+                    Console.WriteLine($"{users[UserKeuze - 1].Naam} toegevoegd als vriend!");
+                }
+            }
+            else if (VriendKeuze == 2)
+            {
+                var Vrienden = client.GetFriends();
+                if (Vrienden == null || Vrienden.Count == 0)
+                {
+                    Console.WriteLine("Je hebt nog geen vrienden.");
+                }
+                else
+                {
+                    client.ShowFriends();
+                    Console.WriteLine("\nKies een nummer om te verwijderen:");
+
+                    int VerwijderKeuze = int.Parse(Console.ReadLine() ?? "0");
+
+                    if (VerwijderKeuze >= 0 && VerwijderKeuze < Vrienden.Count)
+                    {
+                        client.RemoveFriend(VerwijderKeuze);
+                        Console.WriteLine("Vriend verwijderd.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ongeldige keuze.");
+                    }
+                }
+            }
+            else if (VriendKeuze == 3)
+            {
+                client.ShowFriends();
+            }
+            else if (VriendKeuze == 0)
+            {
+                break;
+            }
+        }
     }
     else
     {
